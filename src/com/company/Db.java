449fec868269f9ea1695c28b3a9b58bc3564f08f
Deiv-Dev <nomaxas@gmail.com>
2022-09-23 11:21:35 +0300

    veikia

--- src/com/company/Db.java
+++ src/com/company/Db.java
@@ -1,29 +1,33 @@
 package com.company;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class Db {
 
+public class Db {
     Connection c;
     String URL = "jdbc:mysql://localhost/books";
     String user = "root";
     String password = "";
 
-    public Db() {}
-
     public void connect() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             c = (Connection) DriverManager.getConnection(URL, user, password);
-            System.out.println("Connection success");
+            String sql = "INSERT INTO admin (admin_id, username, password) VALUES (?, ?, ?)";
+            PreparedStatement statement = c.prepareStatement(sql);
+            statement.setString(1,  "1" );
+            statement.setString(2, "Bill");
+            statement.setString(3, "new");
 
-        } catch (SQLException | ClassNotFoundException e){
-            e.printStackTrace();
-            System.out.println("Connection failed");
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new user was inserted successfully!");
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
         }
     }
-
-
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,15 @@
 package com.company;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.SQLException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello world");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-
         Db db = new Db();
         db.connect();
     }
-
-    public static void main(String[] args) { launch(args); }
 }
+
+
